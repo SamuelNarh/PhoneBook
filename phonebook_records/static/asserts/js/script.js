2e3a17aa23adb4b81f22da0e75dfcd2ec58c8b85
Samuel Narh <samuelnarh38@gmail.com>
2023-05-13 07:56:12 +0000

    update

--- phonebook_records/static/asserts/js/script.js
+++ phonebook_records/static/asserts/js/script.js
@@ -1,7 +1,6 @@
 
 
 const mobile_format =document.querySelector('.mobile_format');
-console.log(mobile_format)
 const phone_number = function(){
 
     if (mobile_format.value.length ===3|| mobile_format.value.length ===7)
